0599c866e989c9f502907bb4f2daee2a497155a9
ziwon <ziwon@SEOJIWON>
2025-06-18 21:53:52 +0900

    공지사항 DAO, Mapper

--- src/main/java/com/itwillbs/persistence/NoticeDAO.java
+++ src/main/java/com/itwillbs/persistence/NoticeDAO.java
@@ -1,10 +1,11 @@
 package com.itwillbs.persistence;
 
 import java.util.List;
+
 import com.itwillbs.domain.NoticeVO;
 
 public interface NoticeDAO {
 	
-	List<NoticeVO> getNoticeList();
+	List<NoticeVO> getAllNotices() throws Exception;
 
 }
--- src/main/java/com/itwillbs/persistence/NoticeDAOImpl.java
+++ src/main/java/com/itwillbs/persistence/NoticeDAOImpl.java
@@ -3,20 +3,26 @@ package com.itwillbs.persistence;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.xml.stream.events.Namespace;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
 import com.itwillbs.domain.NoticeVO;
 
 @Repository
-public class NoticeDAOImpl implements NoticeDAO {
+public class NoticeDAOImpl implements NoticeDAO{
 	
 	@Inject
-	private NoticeMapper noticeMapper;
+	private SqlSession sqlSession;
+	
+	// MyBatis 매퍼 namespace
+	private static final String NAMESPACE = "com.itwillbs.mapper.noticeMapper.";
 
 	@Override
-	public List<NoticeVO> getNoticeList() {
-		// TODO Auto-generated method stub
-		return noticeMapper.selectNoticeList();
+	public List<NoticeVO> getAllNotices() throws Exception {
+		// noticeMapper.xml에 정의된 id="getAllNotices" 호출
+		return sqlSession.selectList(NAMESPACE + "getAllNotices");
 	}
 	
 	
