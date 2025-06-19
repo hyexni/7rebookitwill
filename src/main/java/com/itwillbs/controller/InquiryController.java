9e0c99d46d93a0f4a6cced32fa848e838db7f03c
ziwoneee <ziwonee24@gmail.com>
2025-06-19 16:25:14 +0900

    InquiryController 생성 & 컨트롤러 매핑 완료

---
+++ src/main/java/com/itwillbs/controller/InquiryController.java
@@ -0,0 +1,73 @@
+package com.itwillbs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/cs/*")
+public class InquiryController {
+
+	// mylog
+	private static final Logger logger = LoggerFactory.getLogger(InquiryController.class);
+
+	// 서비스 객체를 주입
+	
+	// 기능을 정의
+	
+	// 글쓰기 (정보 입력) / GET
+	@RequestMapping(value = "/write", method = RequestMethod.GET)
+	public void inquiryWriteGET() throws Exception {
+		logger.info(" inquiryWriteGET() 실행 ");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // InquiryController 끝
