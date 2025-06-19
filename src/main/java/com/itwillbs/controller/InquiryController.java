968604497cda4a98b2b91cefe757985f2d93b3d3
ziwoneee <ziwonee24@gmail.com>
2025-06-19 17:38:29 +0900

    글쓰기 정보 처리

--- src/main/java/com/itwillbs/controller/InquiryController.java
+++ src/main/java/com/itwillbs/controller/InquiryController.java
@@ -1,5 +1,7 @@
 package com.itwillbs.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.itwillbs.domain.InquiryVO;
+import com.itwillbs.service.InquiryService;
 
 @Controller
 @RequestMapping(value = "/cs/*")
@@ -16,6 +19,7 @@ public class InquiryController {
 	private static final Logger logger = LoggerFactory.getLogger(InquiryController.class);
 
 	// 서비스 객체를 주입
+	@Inject InquiryService iService;
 	
 	// 기능을 정의
 	
@@ -29,14 +33,20 @@ public class InquiryController {
 	
 	// 글쓰기 (정보 처리) / POST
 	@RequestMapping(value = "/write", method = RequestMethod.POST)
-	public String inquiryWritePOST(InquiryVO vo) {
+	public String inquiryWritePOST(InquiryVO vo) throws Exception {
 		logger.info(" inquiryWritePOST() 실행");
 		
 		// 글쓰기 동작을 처리
 		
 		// 한글처리 인코딩 => web.xml 필터설정
+		// 1) 전달정보(파라메터)를 저장 / 제목, 내용, 고유번호
+		logger.info(" vo : {}", vo);
 		
-		return "";
+		// 서비스기능 -> DAO 기능 -> DB에 저장
+		iService.inquiryWrite(vo);
+		
+		
+		return "redirect:/cs/listAll";
 	}
 	
 	
