2dad8889616e5408d1b9f6d24b8b004a410efbbb
ziwoneee <ziwonee24@gmail.com>
2025-06-19 18:03:31 +0900

    문의분류 컬럼 추가

--- src/main/java/com/itwillbs/domain/InquiryVO.java
+++ src/main/java/com/itwillbs/domain/InquiryVO.java
@@ -8,9 +8,11 @@ public class InquiryVO {
 	
 	private int inquiry_id;				// 문의ID
 	private int member_idx;				// 회원 고유번호
+	private String category; 			// 문의 분류 (이용문의, 배송문의 등)
 	private String title;				// 문의 제목
 	private String content;				// 문의 내용
 	private String status;				// 처리 상태
 	private Timestamp created_at;		// 작성 일시
+
 
 }
