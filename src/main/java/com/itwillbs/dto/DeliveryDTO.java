57080583f89348713508640cb0d6a29210d21277
82105 <82105@DESKTOP-UHCIDRI>
2025-06-26 13:52:03 +0900

    2개 추가

--- src/main/java/com/itwillbs/dto/DeliveryDTO.java
+++ src/main/java/com/itwillbs/dto/DeliveryDTO.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class DeliveryDTO {
-	
-	private int order_id;  // ✅ 꼭 추가해야 함
+
+	private int order_id; // ✅ 꼭 추가해야 함
 	private int member_idx;
 	private String receiver_name;
 	private String receiver_phone;
@@ -13,5 +13,9 @@ public class DeliveryDTO {
 	private String delivery_address;
 	private String address_detail;
 	private String memo;
+
+	// 추가
+	private String status_code; // 배송 상태
+	private String tracking_number; // 운송장 번호
 
 }
